d05f2d0efdfb7758d304befe61f8259c56195935
kasim-yildirim <yildirim.kasimm@gmail.com>
2025-12-04 11:42:16 +0300

    redesign

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -180,7 +180,7 @@ const HeroSection = () => {
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
                 src="https://www.youtube.com/embed/AOV6bqpCV6k?si=IZxCfw4yzYCDY5f_&autoplay=1"
-                title="YouTube video player"{('hero.close')}
+                title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
